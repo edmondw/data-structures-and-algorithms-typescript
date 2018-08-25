fbd58c45314fd22b11a28a41f4c9f17e90842431
Edmond Weiss <edmondweiss@gmail.com>
2018-08-25 19:25:45 -0400

    Added more methods to the list interface

--- src/Lists/interfaces/List.ts
+++ src/Lists/interfaces/List.ts
@@ -1,8 +1,16 @@
 export interface List<E> {
-  size(): number;
-  isEmpty(): boolean;
-  get(i: number);
-  set(i: number, e: E);
   add(i: number, e: E);
+  addFirst(e: E);
+  addLast(e: E);
+  clear();
+  contains(e: E): boolean;
+  get(i: number);
+  indexOf(e: E);
+  isEmpty(): boolean;
+  iterator();
   remove(i: number): E;
+  removeFirst(): E;
+  removeLast(): E;
+  set(i: number, e: E): E;
+  size(): number;
 }
