2c9b9d4cfd216fc8377961f6b32794b0744db367
Edmond Weiss <edmondweiss@gmail.com>
2018-08-26 02:08:34 -0400

    Add missing return values

--- src/Lists/interfaces/List.ts
+++ src/Lists/interfaces/List.ts
@@ -1,16 +1,18 @@
+import ListIterator from './ListIterator';
+
 export interface List<E> {
   add(i: number, e: E);
   addFirst(e: E);
   addLast(e: E);
   clear();
   contains(e: E): boolean;
-  get(i: number);
+  get(i: number): E;
   getFirst(): E;
   getLast(): E;
-  indexOf(e: E);
+  indexOf(e: E): number;
   isEmpty(): boolean;
-  iterator();
-  remove(i: number | E): E;
+  iterator(): ListIterator<E>;
+  remove(i: number): E;
   removeFirst(): E;
   removeLast(): E;
   set(i: number, e: E): E;
