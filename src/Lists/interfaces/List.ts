0a8cef5123d66a066e46c1a8ad316a77d86291a7
Edmond Weiss <edmondweiss@gmail.com>
2018-08-25 20:41:41 -0400

    Allow remove method to accept either an index or an element to remove

--- src/Lists/interfaces/List.ts
+++ src/Lists/interfaces/List.ts
@@ -5,10 +5,12 @@ export interface List<E> {
   clear();
   contains(e: E): boolean;
   get(i: number);
+  getFirst(): E;
+  getLast(): E;
   indexOf(e: E);
   isEmpty(): boolean;
   iterator();
-  remove(i: number): E;
+  remove(i: number | E): E;
   removeFirst(): E;
   removeLast(): E;
   set(i: number, e: E): E;
